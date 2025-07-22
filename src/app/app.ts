d897fbf5c8a8766d86c52ab9b909fbbbda4d915c
Erick Patrick <erick.marinho2003@gmail.com>
2025-07-22 19:21:10 -0300

    chore: add new components into main layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Main } from './components/main/main';
+import { History } from './components/history/history';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Main, History],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

0674abb2cb1dff18144b2c15f1800448e0c03428
Erick Patrick <erick.marinho2003@gmail.com>
2025-07-22 21:17:32 -0300

    feat: add new layout, styles and components

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Main } from './components/main/main';
 import { History } from './components/history/history';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Main, History],
+  imports: [RouterOutlet, Header, Main, History, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

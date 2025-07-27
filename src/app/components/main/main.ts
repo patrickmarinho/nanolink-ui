99baf3638277b30f68bc85f961b9eaf182ed6a45
Erick Patrick <erick.marinho2003@gmail.com>
2025-07-27 03:57:51 -0300

    feat: add new styles and error messages

--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal,} from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './main.html',
   styleUrl: './main.scss'
 })
 export class Main {
+  errorMessage = signal('');
 
+  urlControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('.*\\.com.*')
+  ]);
+
+  shortenUrl() {
+    if (this.urlControl.valid) {
+      console.log('URL a ser encurtada:', this.urlControl.value);
+      this.errorMessage.set('');
+      return;
+    }
+
+    if (this.urlControl.hasError('required')) {
+      this.errorMessage.set('Insira uma URL.');
+    } else if (this.urlControl.hasError('pattern')) {
+      this.errorMessage.set('Insira uma URL válida.');
+    }
+  }
 }

d8b89f2eb493f50174edb752bc87e4d50c01922a
Erick Patrick <erick.marinho2003@gmail.com>
2025-07-27 23:29:18 -0300

    feat: add new method to connect to the backend

--- src/app/service/nano-link-shortener-service.ts
+++ src/app/service/nano-link-shortener-service.ts
@@ -1,9 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface ShortenUrlResponse {
+  shortenedUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NanoLinkShortenerService {
-  private http = inject(HttpClient);
+  private httpClient = inject(HttpClient);
+  private apiUrl = "http://localhost:8080/shorten"
+
+  shortenUrl(url: string): Observable<ShortenUrlResponse>{
+    console.log(url);
+    const body = { originalURL: url }; 
+    return this.httpClient.post<ShortenUrlResponse>( this.apiUrl , body);
+  }
 }
